bf640b913a6c3fe147d91ed6bdec535577fc33b9
kambe.kenta <kenta.k199405@gmail.com>
2020-12-04 22:17:43 +0900

    make TODO struct and display at HTML.

--- hello.go
+++ hello.go
@@ -2,12 +2,55 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
+type Todo struct {
+	CreatedAt       time.Time
+	CreatedAtString string
+	CreatedBy       string
+	Content         string
+	Status          int
+}
+
 func main() {
 	//fmt.Println("hello wolrd!")
+	todo := make([]Todo, 0, 5)
+
+	t1 := Todo{
+		CreatedBy:       "John",
+		Content:         "go to the Gym",
+		Status:          0,
+		CreatedAt:       time.Now(),
+		CreatedAtString: time.Now().Format("2006-01-02 15:04:05"),
+	}
+	t2 := Todo{
+		CreatedBy:       "Mike",
+		Content:         "watch movies",
+		Status:          0,
+		CreatedAt:       time.Now(),
+		CreatedAtString: time.Now().Format("2006-01-02 15:04:05"),
+	}
+	t3 := Todo{
+		CreatedBy:       "Harry",
+		Content:         "go shopping",
+		Status:          1,
+		CreatedAt:       time.Now(),
+		CreatedAtString: time.Now().Format("2006-01-02 15:04:05"),
+	}
+	t4 := Todo{
+		CreatedBy:       "Steve",
+		Content:         "wash a car",
+		Status:          0,
+		CreatedAt:       time.Now(),
+		CreatedAtString: time.Now().Format("2006-01-02 15:04:05"),
+	}
+	todo = append(todo, t1)
+	todo = append(todo, t2)
+	todo = append(todo, t3)
+	todo = append(todo, t4)
 
 	r := gin.Default()
 	r.LoadHTMLFiles("./template/index.html")
@@ -17,7 +60,9 @@ func main() {
 		})
 	})
 	r.GET("/todo", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", map[string]interface{}{})
+		c.HTML(http.StatusOK, "index.html", map[string]interface{}{
+			"todo": todo,
+		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
